8d9b299c96b0e1cce3c3a22ba9add945af3a8a7d
maparisti <pauline.aristizabal@gmail.com>
2022-08-10 00:14:08 -0500

    Entrega Arrays

---
+++ arrays.js
@@ -0,0 +1,47 @@
+debugger
+
+let dolar = 1.02
+let euro = 0.98
+let bienvenida = prompt("Estás listo para convertir tu dinero? Da click en continuar para comenzar")
+let escoger = prompt("Conertir USD a EUR o EUR a USD?")
+const divisa = ['usd a eur', 'eur a usd'];
+
+function valorEUR() {
+    divisa = parseFloat(prompt("Ingresa el valor a convertir:"))
+    resultado = usdEur(divisa, euro)
+    console.log(divisa, "dolares", "equivale a:", resultado, "euros")
+}
+
+function valorUSD() {
+    divisa = parseFloat(prompt("Ingresa el valor a convertir:"))
+    resultado = usdEur(divisa, dolar)
+    console.log(divisa, "euros", "equivale a:", resultado, "dolares")
+}
+
+function usdEur(divisa, euro) {
+    return divisa * euro
+}
+
+function eurUsd (divisa, dolar) {
+    return divisa * dolar
+}
+
+function nuevaConversion(){
+    let nuevaCon = prompt("Ingresa nuevamente la conversion, por favor")
+    let resultado = divisa.includes(nuevaCon)
+    if (!resultado) {
+        divisa.push(nuevaCon)
+        console.log("Gracias!", nuevaCon, "ha sido añadida cómo sugerencia")
+    }
+}
+switch (escoger.toLowerCase()) {
+    case "usd a eur":
+        valorEUR()
+        break
+    case "eur a usd":
+        valorUSD()
+        break
+    default:
+        console.warn("No tenemos disponible esa conversion, pero puedes ingresar tu sugerencia a continuación")
+        nuevaConversion()
+}
